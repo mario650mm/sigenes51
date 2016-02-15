bf99ccd64f7112868f306859994a4fc8fca100b4
JorgeNaranjo90 <jorge020890@gmail.com>
2016-02-14 21:49:56 -0600

    [ADD] Added models

---
+++ sigenes/app/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    protected $table='posts';
+
+    protected $fillable=[];
+}
