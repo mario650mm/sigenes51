31f21b091987e8a8b0551d87668db12af4bf238e
mario650mm <mario650m@gmail.com>
2016-02-28 19:55:58 -0600

    Generate new teachers route file by Mario

---
+++ sigenes/app/Http/routes/teachers.php
@@ -0,0 +1,19 @@
+<?php
+
+// Views routes
+Route::group(['middleware' => ['auth', 'admin']], function () {
+    Route::resource('teachers', 'TeacherController',
+        ['only' => ['index', 'create', 'edit']]);
+
+});
+
+// API routes
+
+Route::group(['prefix' => 'api/v1', 'middleware' => ['auth', 'admin']], function () {
+
+    Route::post('teachers', 'TeacherController@store');
+    Route::put('teachers', 'TeacherController@update');
+    Route::delete('teachers/{id}', 'TeacherController@destroy');
+    Route::get('teachers', 'TeacherController@getAllData');
+
+});
