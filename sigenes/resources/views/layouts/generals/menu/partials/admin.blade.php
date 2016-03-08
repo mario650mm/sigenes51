f36e132cbae457151728e27ae79c24512b7a2191
mario650mm <mario650m@gmail.com>
2016-03-07 20:25:15 -0600

    Modify the admin vertical menu and add two files of lang for partners module

--- sigenes/resources/views/layouts/generals/menu/partials/admin.blade.php
+++ sigenes/resources/views/layouts/generals/menu/partials/admin.blade.php
@@ -3,6 +3,10 @@
     <div class="menu">
         <a class="item" href="{{ route('users.index') }}">{{ trans('users.title') }}</a>
     </div>
+	<div class="header"><i class="fa fa-users"></i>	@lang('partners.title')</div>
+	<div class="menu">
+		<a class="item" href="{{ route('partners.index') }}">	@lang('partners.title')</a>
+	</div>
 	<div class="header"><i class="fa fa-user-secret"></i> @lang('teachers.title')</div>
 	<div class="menu">
 		<a class="item" href="{{ route('teachers.index') }}">   @lang('teachers.title')</a>
