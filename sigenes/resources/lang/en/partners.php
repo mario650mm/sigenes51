e93533bfc6e2561fa9b5e7cfad89c9f7a1055992
mario650mm <mario650m@gmail.com>
2016-03-08 10:23:43 -0600

    Modify lang partners and add the list view by Mario

--- sigenes/resources/lang/en/partners.php
+++ sigenes/resources/lang/en/partners.php
@@ -1,4 +1,20 @@
 <?php
-return[
-  "title" => "Partners"
+return [
+    "title" => "Partners",
+    "partner" => "Partner",
+    "last_name" => "Last name",
+    "s_last_name" => "Second last name",
+    "curp" => "CURP",
+    "birthdate" => "Birthdate",
+    "sex" => "Sex",
+    "email1" => "Email 1",
+    "email2" => "Email 2",
+    "nationality" => "Nationality",
+    "telephone" => "Telephone",
+    "cellphone" => "Cellphone",
+    "marital_status" => "Marital status",
+    "create_partner" => "Create partner",
+    "edit_partner" => "Edit partner",
+    "delete_partner" => "delete partner",
+
 ];
--- sigenes/resources/lang/es/partners.php
+++ sigenes/resources/lang/es/partners.php
@@ -1,4 +1,19 @@
 <?php
 return[
-    "title" => "Socios"
+    "title" => "Socios",
+    "partner" => "Socio",
+    "last_name" => "Apellido 1",
+    "s_last_name" => "Apellido 2",
+    "curp" => "CURP",
+    "birthdate" => "Fecha de nacimiento",
+    "sex" => "Sexo",
+    "email1" => "Correo 1",
+    "email2" => "Correo 2",
+    "nationality" => "Nationalidad",
+    "telephone" => "Teléfono",
+    "cellphone" => "Celular",
+    "marital_status" => "Estado civil",
+    "create_partner" => "Crear Socio",
+    "edit_partner" => "Editar Socio",
+    "delete_partner" => "Eliminar Socio",
 ];
