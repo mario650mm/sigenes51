895c8275e61923d93d5c9fe374b41082fe857e62
mario650mm <mario650m@gmail.com>
2016-03-14 13:53:58 -0600

    Add employee lang by Mario

--- sigenes/resources/views/layouts/generals/menu/partials/admin.blade.php
+++ sigenes/resources/views/layouts/generals/menu/partials/admin.blade.php
@@ -7,7 +7,7 @@
 	<div class="menu">
 		<a class="item" href="{{ route('partners.index') }}">	@lang('partners.title')</a>
 	</div>
-	<div class="header"><i class="fa fa-user-secret"></i> @lang('teachers.title')</div>
+	<div class="header"><i class="fa fa-user-secret"></i>@lang('employees.title')</div>
 	<div class="menu">
 		<a class="item" href="{{ route('teachers.index') }}">   @lang('teachers.title')</a>
 	</div>
--- sigenes/resources/views/layouts/generals/menu/partials/employee.blade.php
+++ sigenes/resources/views/layouts/generals/menu/partials/employee.blade.php
@@ -6,7 +6,7 @@
     <div class="menu">
         <a class="item" href="{{ route('admin.records.index')}}"> School records</a>
     </div>
-    <div class="header"><i class="fa fa-user-secret"></i> Empleados</div>
+    <div class="header"><i class="fa fa-user-secret"></i>@lang('employees.title')</div>
     <div class="menu">
         <a class="item" href="{{ route('teachers.index') }}">   @lang('teachers.title')</a>
     </div>
