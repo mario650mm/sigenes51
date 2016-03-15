1478f1aca3488029617a37c3bb5e75f99b92d3ec
mario650mm <mario650m@gmail.com>
2016-03-14 20:45:38 -0600

    Modify scripts.blade.php by Mario

--- sigenes/resources/views/layouts/generals/scripts.blade.php
+++ sigenes/resources/views/layouts/generals/scripts.blade.php
@@ -45,27 +45,20 @@
     <script src="{{ asset('js/script/services/schoolrecord/schoolrecord.factories.js') }}"></script>
     <script src="{{ asset('js/script/services/schoolrecord/schoolrecordType.factories.js') }}"></script>
     <script src="{{ asset('js/script/services/applicants/applicant.factories.js') }}"></script>
-<<<<<<< HEAD
     <script src="{{ asset('js/script/services/partners/partners.factories.js') }}"></script>
     <script src="{{ asset('js/script/services/teachers/teachers.factories.js') }}"></script>
     <script src="{{ asset('js/script/services/employees/employees.factories.js') }}"></script>
-=======
     <script src="{{ asset('js/script/services/period/period.factories.js') }}"></script>
->>>>>>> origin/produccion
 
     <!-- Angular Controllers -->
     <script src="{{ asset('js/script/controllers/users/usersCtrl.js') }}"></script>
     <script src="{{ asset('js/script/controllers/low/suspencionsAdminCtrl.js') }}"></script>
     <script src="{{ asset('js/script/controllers/applicants/applicantsController.js') }}"></script>
     <script src="{{ asset('js/script/controllers/schoolrecord/schoolrecordAdminCtrl.js') }}"></script>
-<<<<<<< HEAD
     <script src="{{ asset('js/script/controllers/partners/partnersCtrl.js') }}"></script>
     <script src="{{ asset('js/script/controllers/teachers/teachersCtrl.js') }}"></script>
 @endif
-
-
-=======
     <script src="{{ asset('js/script/controllers/schoolrecord/schoolrecordTypeAdminCtrl.js') }}"></script>
     <script src="{{ asset('js/script/controllers/period/periodAdminCtrl.js') }}"></script>
->>>>>>> origin/produccion
+
 
