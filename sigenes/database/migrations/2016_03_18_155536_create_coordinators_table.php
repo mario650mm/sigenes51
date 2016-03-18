afc87a5e1a5ea9af53b4954ae4864d27a943bc89
mario650mm <mario650m@gmail.com>
2016-03-18 09:56:14 -0600

    generate new coordinator model and migration by Mario

---
+++ sigenes/database/migrations/2016_03_18_155536_create_coordinators_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoordinatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coordinators', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('coordinators');
+    }
+}
