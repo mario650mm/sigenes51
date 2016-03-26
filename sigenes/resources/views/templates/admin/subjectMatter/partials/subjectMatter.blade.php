4ad69386e59f6fe4340fe920ab2bf294511fad08
jjtristan <you@example.com>
2016-03-25 20:22:50 -0600

    Change interfaz subjectmatter

--- sigenes/resources/views/templates/admin/subjectMatter/partials/subjectMatter.blade.php
+++ sigenes/resources/views/templates/admin/subjectMatter/partials/subjectMatter.blade.php
@@ -54,28 +54,43 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-6">
-			<div class="form-group">
-				<!-- <label class="label-control col-lg-4 col-sm-4 col-md-4 col-xs-4">Semestre:</label> -->
-				<div class="row col-lg-6 col-sm-6 col-md-6 col-xs-6">
-					<div class="row">
-						<input type="radio" id="isoptional" name="isoptional" ng-model="subjectmatter.isopctional" ng-init="subjectmatter.isopctional=false"/><label class="label-control">&nbsp;&nbsp;&nbsp;Asignatura opcional</label>
-					</div>
-					<div class="row">
-						<input type="radio" id="isoptional" name="isoptional" ng-model="subjectmatter.isopctional" ng-init="subjectmatter.isopctional=false"/><label class="label-control">&nbsp;&nbsp;&nbsp;Asignatura obligatoria</label>
-					</div>
+		
+	</div>
+</div>
+<br>
+<div class="row">
+
+	<div class="col-lg-8 col-sm-8 col-md-8 col-xs-8 col-lg-offset-0 col-sm-offset-0 col-md-offset-0 col-xs-offset-0">
+		<div class="form-group">
+			<!-- <label class="label-control col-lg-4 col-sm-4 col-md-4 col-xs-4">Semestre:</label> -->
+			<div class="row col-lg-4 col-sm-4 col-md-4 col-xs-4 col-lg-offset-1 col-sm-offset-1 col-md-offset-1 col-xs-offset-1">
+				<div class="row">
+					<input type="radio" id="isoptional" name="isoptional" ng-model="subjectmatter.isopctional" ng-init="subjectmatter.isopctional=false"/><label class="label-control">&nbsp;&nbsp;&nbsp;Asignatura opcional</label>
 				</div>
-				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-6">
-					<div class="row">
-						<input type="radio" id="subjectmatter" name="study" ng-model="subjectmatter.iscomun_deep"  ng-click="changeComun()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Es de tronco común</label>
-					</div>
-					<div class="row">
-						<input type="radio" id="deepingarea" name="study" ng-model="subjectmatter.isdeepending" ng-init="subjectmatter.isdeepending=false" ng-click="change()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Es de profundización</label> 
-					</div>
-					<div class="row">
-						<input type="radio" id="technique" name="study" ng-model="subjectmatter.istechnique"ng-init="subjectmatter.istechnique=false" ng-click="changeComun()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Es Carrera tecnica</label>
-					</div>
+				<div class="row">
+					<input type="radio" id="isoptional" name="isoptional" ng-model="subjectmatter.isopctional" ng-init="subjectmatter.isopctional=false"/><label class="label-control">&nbsp;&nbsp;&nbsp;Asignatura obligatoria</label>
 				</div>
+			</div>
+			<div class="col-lg-6 col-sm-6 col-md-6 col-xs-6 col-lg-offset-1 col-sm-offset-1 col-md-offset-1 col-xs-offset-1">
+				<div class="row">
+					<input type="radio" id="subjectmatter" name="study" ng-model="subjectmatter.iscomun_deep"  ng-click="changeComun()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Tronco común</label>
+				</div>
+				<div class="row">
+					<input type="radio" id="deepingarea" name="study" ng-model="subjectmatter.isdeepending" ng-init="subjectmatter.isdeepending=false" ng-click="change()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Profundización</label> 
+				</div>
+				<div class="row">
+					<input type="radio" id="technique" name="study" ng-model="subjectmatter.istechnique"ng-init="subjectmatter.istechnique=false" ng-click="changeComun()"/><label class="label-control">&nbsp;&nbsp;&nbsp;Carrera tecnica</label>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-4 col-sm-4 col-md-4 col-xs-4 ">
+		<div class="form-group">
+			<div class="row">
+				<input type="radio" name="optradio"><label class="label-control">&nbsp;&nbsp;&nbsp;Seriacion obligatoria</label>
+			</div>
+			<div class="row">
+				<input type="radio" name="optradio"><label class="label-control">&nbsp;&nbsp;&nbsp;Seriacion indicativa</label>
 			</div>
 		</div>
 	</div>
@@ -158,16 +173,7 @@
 <br>
 <div class="row">
 	<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-		<div class="col-lg-8 col-sm-8 col-md-8 col-xs-8">
-			<div class="form-group">
-				<div class="row">
-					<input type="radio" name="optradio"><label class="label-control">&nbsp;&nbsp;&nbsp;Seriacion obligatoria</label>
-				</div>
-				<div class="row">
-					<input type="radio" name="optradio"><label class="label-control">&nbsp;&nbsp;&nbsp;Seriacion indicativa</label>
-				</div>
-			</div>
-		</div>
+		
 		
 	</div>
 </div>
