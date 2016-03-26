0e58e7f5b9bcaff83bbd7acfec9261a21d375cd5
JorgeNaranjo90 <jorge020890@gmail.com>
2016-03-25 20:16:36 -0600

    [ADD] Added years model, migration and function in main factory, update semesters migration add year_id for relationship

--- sigenes/database/migrations/2016_03_13_021808_create_half_years_table.php
+++ sigenes/database/migrations/2016_03_13_021808_create_half_years_table.php
@@ -15,42 +15,53 @@ class CreateHalfYearsTable extends Migration
         Schema::create('semesters', function(Blueprint $table){
             $table->increments('id');
             $table->string('name', 30);
+            $table->integer('year_id')->unsigned();
             $table->timestamps();
             $table->softDeletes();
+            $table->foreign('year_id')->references('id')
+                ->on('years')->onDelete('cascade')->onUpdte('cascade');
         });
 
         $now = date('Y-m-d H:i:s');
         \DB::table('semesters')->insert([
             [
                 'name' => 'Primer Semestre',
+                'year_id' => 1,
                 'created_at' => $now
             ],
             [
                 'name' => 'Segundo Semestre',
+                'year_id' => 1,
                 'created_at' => $now            
             ],
             [
                 'name' => 'Tercer Semestre',
+                'year_id' => 2,
                 'created_at' => $now            
             ],
             [
                 'name' => 'Cuarto Semestre',
+                'year_id' => 2,
                 'created_at' => $now  
             ],
             [
                 'name' => 'Quinto Semestre',
+                'year_id' => 3,
                 'created_at' => $now  
             ],
             [
                 'name' => 'Sexto Semestre',
+                'year_id' => 3,
                 'created_at' => $now  
             ],
             [
                 'name' => 'Septimo Semestre',
+                'year_id' => 4,
                 'created_at' => $now  
             ],
             [
                 'name' => 'Octavo Semestre',
+                'year_id' => 4,
                 'created_at' => $now  
             ]
         ]);
