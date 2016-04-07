a6643b2580fa1cd59cf5e76ee65e075fcf057bef
JorgeNaranjo90 <jorge020890@gmail.com>
2016-04-06 22:40:38 -0500

    Update fields account number and nss required

--- sigenes/public/js/script/directives/directiveEnesAuth.js
+++ sigenes/public/js/script/directives/directiveEnesAuth.js
@@ -244,6 +244,25 @@ angular.module('EnesAuth')
             }
         }
     })
+    .directive('nssValidate', function() {
+        return {
+            restrice: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+                var numberregex =  /^[0-9]{11}$/;
+                ngModel.$parsers.unshift(function(viewValue){
+                    if(numberregex.test(viewValue)) {
+                        ngModel.$setValidity('numbercheck', true);
+                        return viewValue;
+                    }
+                    else{
+                        ngModel.$setValidity('numbercheck', false);
+                        return undefined;
+                    }
+                });
+            }
+        }
+    })
     .directive('anioValidate', function() {
         return {
             restrice: 'A',
